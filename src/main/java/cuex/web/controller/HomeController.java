add585056b8aa56d059500f0653a1e38f506b306
Denis <dev.szczukocki.denis@gmail.com>
2018-08-12 21:18:34 +0200

    initial webapp structure

---
+++ src/main/java/cuex/web/controller/HomeController.java
@@ -0,0 +1,22 @@
+package cuex.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping
+    public List<String> greetings() {
+        ArrayList<String> greetings = new ArrayList<>();
+        greetings.add("Bonjour!");
+        greetings.add("Hello!");
+        greetings.add("Dzień dobry!");
+        return greetings;
+    }
+}
