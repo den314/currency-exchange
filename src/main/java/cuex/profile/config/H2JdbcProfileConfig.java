a11fe1823ca778b5f16be3b8ae72e37f4a58d90b
Denis Szczukocki <Denis_Szczukocki@epam.com>
2018-08-16 10:55:45 +0200

    pkg rename, change HomeController mapping

--- src/main/java/cuex/profile/config/H2JdbcProfileConfig.java
+++ src/main/java/cuex/profile/config/H2JdbcProfileConfig.java
@@ -1,4 +1,4 @@
-package cuex.config;
+package cuex.profile.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/cuex/web/controller/HomeController.java
+++ src/main/java/cuex/web/controller/HomeController.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/")
 public class HomeController {
 
     @GetMapping
